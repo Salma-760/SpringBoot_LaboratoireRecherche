9750dde53ab8ec2b0c336ac643c59c92bdd828e4
bouchrakhalfaouy <bkhalfaouy@gmail.com>
2025-06-26 16:25:18 +0100

    Ajout des pages statiques

---
+++ SpringBoot-React/Spring_BackEnd/demo/src/main/java/com/example/demo/repository/MembreEquipeRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.MembreEquipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MembreEquipeRepository extends JpaRepository<MembreEquipe, Long> {
+ 
+}
