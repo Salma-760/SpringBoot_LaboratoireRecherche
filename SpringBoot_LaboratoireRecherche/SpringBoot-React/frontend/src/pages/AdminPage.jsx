19ce74542198fa7c004d3e81519367159c84a6be
bouchrakhalfaouy <bkhalfaouy@gmail.com>
2025-06-28 20:06:49 +0100

    Résolution des conflits après pull

--- SpringBoot_LaboratoireRecherche/SpringBoot-React/frontend/src/pages/AdminPage.jsx
+++ SpringBoot_LaboratoireRecherche/SpringBoot-React/frontend/src/pages/AdminPage.jsx
@@ -1,25 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import { useNavigate } from "react-router-dom";
-
-const AdminPage = () => {
-    const navigate = useNavigate();
-
-    const handleLogout = () => {
-        localStorage.removeItem("token"); // supprime le token
-        navigate("/"); // redirige vers la page d'accueil ou login
-    };
-
-    return (
-        <div>
-            <h1>Page Admin</h1>
-            {/* ... contenu Admin ... */}
-            <button onClick={handleLogout} className="btn-logout">
-                Déconnexion
-            </button>
-        </div>
-    );
-=======
 import SidebarAdmin from "./SideBarAdmin"; // à créer
 import { Outlet } from "react-router-dom";
 
@@ -32,7 +11,6 @@ const AdminPage = () => {
       </main>
     </div>
   );
->>>>>>> b5af526d34fc44be4eb472631b6e4dbd4c058764
 };
 
 export default AdminPage;
